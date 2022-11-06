16af27f40a46de98579eadfec81cace17ac14325
Bentsiwa <amabbentil@gmail.com>
2022-11-06 18:31:40 +0000

    animations

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// The code says listen for a click on the class question
+// If you hear a click, toggle the class active if the parent node- 
+//question-and-answers has an active class in any of its children
+//If you dont hear a click on any of the questions remove the class active 
+//And add active to the newly presed quesiton
+  
+  
+  
 const questions = document.querySelectorAll(".question");
 
 questions.forEach((question) => 
@@ -5,20 +13,21 @@ questions.forEach((question) =>
         
         if(question.parentNode.classList.contains("active")) {
         
-           question.parentNode.classList.toggle("active")
-        }
+           question.parentNode.classList.toggle("active");
+         
+           
 
+        }
+   
         else {
-            questions.forEach(question => question.parentNode.classList.remove("active"))
-            question.parentNode.classList.add("active")
+            //This ensures not more than one answer is opened
+            questions.forEach(questionItem => questionItem.parentNode.classList.remove("active")) 
+            question.parentNode.classList.add("active");
+                     
         }
 
 
     })  
 )  
-
-
-
-
 
 
